879b0554b177e606739be389471d918585d9857c
Emery2100 <Emery2100@yahoo.com>
2025-09-18 20:26:29 -0700

    final

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import PostListView
+from .views import PostCreateView, PostDeleteView, PostListView, PostDetailView, PostUpdateView
 
 urlpatterns = [
-    path('list/', PostListView.as_view(), name='posts')
+    path('list/', PostListView.as_view(), name='posts'),
+    path('<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('new/', PostCreateView.as_view(), name='post_new'),
+    path('<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
+    path('<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),
 ]
+
+
--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,76 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import ( 
-    ListView
+    ListView, 
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
 )
 from .models import Post
+from django.contrib.auth.models import User
+
+
+# Crud -> Create, Read, Update and Delete
+# The generic classes are ListView, CreateView, UpdateView, DeleteView and DetailView
+#                          GET       POST         POST        POST           GET
+
 # Create your views here.
 
+"""
+PostListView is going to retrieve all of the object from the Post table in the DB
+"""
 class PostListView(ListView):
+    # template_name attribute is going to render a specific html file
     template_name = "posts/list.html"
+    #model is going to  be from which table we want to retrieve the data
     model = Post
+
+    #queryset is used to define the model used for the view
+    #queryset = Post.objects.all()
+
+    # context is a python dictionary that holds the data for the generic views
+    #and this context travels to the htmls
+    #by default the context name of ListView and DetailView is "Object" or "object_list"
+
+    #context_object_name would allow us to modify the name and how to call it in the htmls
     context_object_name = "post_list"
+
+"""
+PostDetailView is going to retrieve a single object from the Post table in the DB
+"""
+class PostDetailView(DetailView):
+    template_name = "posts/detail.html"
+    model = Post
+    context_object_name = "single_post"
+
+"""
+PostCreateView is going to allow us to create a new post and add it to the DB
+"""
+class PostCreateView(CreateView):
+    template_name = "posts/new.html"
+    model = Post
+    fields = ['title', 'subtitle', 'body']
+
+    def form_valid(self, form):
+        print(form)
+        print(User.objects.all())
+        form.instance.author = User.objects.filter(is_superuser=True).first()
+        return super().form_valid(form)
+
+"""
+PostUpdateView is going to allow us to update an existing record in the DB
+"""
+
+class PostDeleteView(DeleteView):
+    template_name = "posts/delete.html"
+    model = Post
+    success_url = reverse_lazy('posts')
+
+    """
+    PostUpdateView is going to allow us to update an existing record in the DB
+    """
+
+class PostUpdateView(UpdateView):
+    template_name = "posts/edit.html"
+    model = Post
+    fields = ['title', 'subtitle', 'body']
